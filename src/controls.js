887b8a0be331d627e134066ba8d4ae5d48753384
Thomas Fuchs <thomas@fesch.at>
2005-07-30 10:13:17 +0000

    script.aculo.us: controls.js refactoring
    git-svn-id: http://svn.rubyonrails.org/rails/spinoffs/scriptaculous@1954 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- src/controls.js
+++ src/controls.js
@@ -47,7 +47,7 @@ Element.collectTextNodesIgnoreClass = function(element, ignoreclass) {
 // a getUpdatedChoices function that will be invoked every time
 // the text inside the monitored textbox changes. This method 
 // should get the text for which to provide autocompletion by
-// invoking this.getEntry(), NOT by directly accessing
+// invoking this.getToken(), NOT by directly accessing
 // this.element.value. This is to allow incremental tokenized
 // autocompletion. Specific auto-completion logic (AJAX, etc)
 // belongs in getUpdatedChoices.
@@ -58,7 +58,7 @@ Element.collectTextNodesIgnoreClass = function(element, ignoreclass) {
 // new Ajax.Autocompleter('id','upd', '/url/', { tokens: ',' });
 // will incrementally autocomplete with a comma as the token.
 // Additionally, ',' in the above example can be replaced with
-// a token array, e.g. { tokens: new Array (',', '\n') } which
+// a token array, e.g. { tokens: [',', '\n'] } which
 // enables autocompletion on multiple tokens. This is most 
 // useful when one of the tokens is \n (a newline), as it 
 // allows smart autocompletion after linebreaks.
@@ -66,31 +66,31 @@ Element.collectTextNodesIgnoreClass = function(element, ignoreclass) {
 var Autocompleter = {}
 Autocompleter.Base = function() {};
 Autocompleter.Base.prototype = {
-  base_initialize: function(element, update, options) {
+  baseInitialize: function(element, update, options) {
     this.element     = $(element); 
     this.update      = $(update);  
-    this.has_focus   = false; 
+    this.hasFocus    = false; 
     this.changed     = false; 
     this.active      = false; 
     this.index       = 0;     
-    this.entry_count = 0;
+    this.entryCount  = 0;
 
     if (this.setOptions)
       this.setOptions(options);
     else
       this.options = options || {};
      
-    this.options.tokens       = this.options.tokens || new Array();
+    this.options.tokens       = this.options.tokens || [];
     this.options.frequency    = this.options.frequency || 0.4;
-    this.options.min_chars    = this.options.min_chars || 1;
+    this.options.minChars     = this.options.minChars || 1;
     this.options.onShow       = this.options.onShow || 
     function(element, update){ 
       if(!update.style.position || update.style.position=='absolute') {
         update.style.position = 'absolute';
-          var offsets = Position.cumulativeOffset(element);
-          update.style.left = offsets[0] + 'px';
-          update.style.top  = (offsets[1] + element.offsetHeight) + 'px';
-          update.style.width = element.offsetWidth + 'px';
+        var offsets = Position.cumulativeOffset(element);
+        update.style.left = offsets[0] + 'px';
+        update.style.top  = (offsets[1] + element.offsetHeight) + 'px';
+        update.style.width = element.offsetWidth + 'px';
       }
       new Effect.Appear(update,{duration:0.15});
     };
@@ -112,8 +112,8 @@ Autocompleter.Base.prototype = {
   },
 
   show: function() {
-    if(this.update.style.display=='none') this.options.onShow(this.element, this.update);
-    if(!this.iefix && (navigator.appVersion.indexOf('MSIE')>0) && this.update.style.position=='absolute') {
+    if(Element.getStyle(this.update, 'display')=='none') this.options.onShow(this.element, this.update);
+    if(!this.iefix && (navigator.appVersion.indexOf('MSIE')>0) && (Element.getStyle(this.update, 'position')=='absolute')) {
       new Insertion.After(this.update, 
        '<iframe id="' + this.update.id + '_iefix" '+
        'style="display:none;filter:progid:DXImageTransform.Microsoft.Alpha(opacity=0);" ' +
@@ -129,7 +129,7 @@ Autocompleter.Base.prototype = {
   },
   
   hide: function() {
-    if(this.update.style.display=='') this.options.onHide(this.element, this.update);
+    if(Element.getStyle(this.update, 'display')!='none') this.options.onHide(this.element, this.update);
     if(this.iefix) Element.hide(this.iefix);
   },
   
@@ -146,7 +146,7 @@ Autocompleter.Base.prototype = {
       switch(event.keyCode) {
        case Event.KEY_TAB:
        case Event.KEY_RETURN:
-         this.select_entry();
+         this.selectEntry();
          Event.stop(event);
        case Event.KEY_ESC:
          this.hide();
@@ -156,12 +156,12 @@ Autocompleter.Base.prototype = {
        case Event.KEY_RIGHT:
          return;
        case Event.KEY_UP:
-         this.mark_previous();
+         this.markPrevious();
          this.render();
          if(navigator.appVersion.indexOf('AppleWebKit')>0) Event.stop(event);
          return;
        case Event.KEY_DOWN:
-         this.mark_next();
+         this.markNext();
          this.render();
          if(navigator.appVersion.indexOf('AppleWebKit')>0) Event.stop(event);
          return;
@@ -171,7 +171,7 @@ Autocompleter.Base.prototype = {
         return;
     
     this.changed = true;
-    this.has_focus = true;
+    this.hasFocus = true;
     
     if(this.observer) clearTimeout(this.observer);
       this.observer = 
@@ -191,88 +191,88 @@ Autocompleter.Base.prototype = {
   onClick: function(event) {
     var element = Event.findElement(event, 'LI');
     this.index = element.autocompleteIndex;
-    this.select_entry();
+    this.selectEntry();
     Event.stop(event);
   },
   
   onBlur: function(event) {
     // needed to make click events working
     setTimeout(this.hide.bind(this), 250);
-    this.has_focus = false;
+    this.hasFocus = false;
     this.active = false;     
   }, 
   
   render: function() {
-    if(this.entry_count > 0) {
-      for (var i = 0; i < this.entry_count; i++)
+    if(this.entryCount > 0) {
+      for (var i = 0; i < this.entryCount; i++)
         this.index==i ? 
-          Element.addClassName(this.get_entry(i),"selected") : 
-          Element.removeClassName(this.get_entry(i),"selected");
+          Element.addClassName(this.getEntry(i),"selected") : 
+          Element.removeClassName(this.getEntry(i),"selected");
         
-      if(this.has_focus) { 
-        if(this.get_current_entry().scrollIntoView) 
-          this.get_current_entry().scrollIntoView(false);
-        
+      if(this.hasFocus) { 
         this.show();
         this.active = true;
       }
     } else this.hide();
   },
   
-  mark_previous: function() {
+  markPrevious: function() {
     if(this.index > 0) this.index--
-      else this.index = this.entry_count-1;
+      else this.index = this.entryCcount-1;
   },
   
-  mark_next: function() {
-    if(this.index < this.entry_count-1) this.index++
+  markNext: function() {
+    if(this.index < this.entryCount-1) this.index++
       else this.index = 0;
   },
   
-  get_entry: function(index) {
+  getEntry: function(index) {
     return this.update.firstChild.childNodes[index];
   },
   
-  get_current_entry: function() {
-    return this.get_entry(this.index);
+  getCurrentEntry: function() {
+    return this.getEntry(this.index);
   },
   
-  select_entry: function() {
+  selectEntry: function() {
     this.active = false;
-    value = Element.collectTextNodesIgnoreClass(this.get_current_entry(), 'informal').unescapeHTML();
-    this.updateElement(value);
+    this.updateElement(this.getCurrentEntry());
     this.element.focus();
   },
 
-  updateElement: function(value) {
-    var last_token_pos = this.findLastToken();
-    if (last_token_pos != -1) {
-      var new_value = this.element.value.substr(0, last_token_pos + 1);
-      var whitespace = this.element.value.substr(last_token_pos + 1).match(/^\s+/);
+  updateElement: function(selectedElement) {
+    if (this.options.updateElement)
+      return(this.options.updateElement(selectedElement));
+      
+    var value = Element.collectTextNodesIgnoreClass(selectedElement, 'informal').unescapeHTML();
+    var lastTokenPos = this.findLastToken();
+    if (lastTokenPos != -1) {
+      var newValue = this.element.value.substr(0, lastTokenPos + 1);
+      var whitespace = this.element.value.substr(lastTokenPos + 1).match(/^\s+/);
       if (whitespace)
-        new_value += whitespace[0];
-      this.element.value = new_value + value;
+        newValue += whitespace[0];
+      this.element.value = newValue + value;
     } else {
       this.element.value = value;
     } 
   },
   
   updateChoices: function(choices) {
-    if(!this.changed && this.has_focus) {
+    if(!this.changed && this.hasFocus) {
       this.update.innerHTML = choices;
       Element.cleanWhitespace(this.update);
       Element.cleanWhitespace(this.update.firstChild);
 
       if(this.update.firstChild && this.update.firstChild.childNodes) {
-        this.entry_count = 
+        this.entryCount = 
           this.update.firstChild.childNodes.length;
-        for (var i = 0; i < this.entry_count; i++) {
-          entry = this.get_entry(i);
+        for (var i = 0; i < this.entryCount; i++) {
+          var entry = this.getEntry(i);
           entry.autocompleteIndex = i;
           this.addObservers(entry);
         }
       } else { 
-        this.entry_count = 0;
+        this.entryCount = 0;
       }
       
       this.stopIndicator();
@@ -289,7 +289,7 @@ Autocompleter.Base.prototype = {
 
   onObserverEvent: function() {
     this.changed = false;   
-    if(this.getEntry().length>=this.options.min_chars) {
+    if(this.getToken().length>=this.options.minChars) {
       this.startIndicator();
       this.getUpdatedChoices();
     } else {
@@ -298,10 +298,10 @@ Autocompleter.Base.prototype = {
     }
   },
 
-  getEntry: function() {
-    var token_pos = this.findLastToken();
-    if (token_pos != -1)
-      var ret = this.element.value.substr(token_pos + 1).replace(/^\s+/,'').replace(/\s+$/,'');
+  getToken: function() {
+    var tokenPos = this.findLastToken();
+    if (tokenPos != -1)
+      var ret = this.element.value.substr(tokenPos + 1).replace(/^\s+/,'').replace(/\s+$/,'');
     else
       var ret = this.element.value;
     
@@ -309,21 +309,21 @@ Autocompleter.Base.prototype = {
   },
 
   findLastToken: function() {
-    var last_token_pos = -1;
+    var lastTokenPos = -1;
 
     for (var i=0; i<this.options.tokens.length; i++) {
-      var this_token_pos = this.element.value.lastIndexOf(this.options.tokens[i]);
-      if (this_token_pos > last_token_pos)
-        last_token_pos = this_token_pos;
+      var thisTokenPos = this.element.value.lastIndexOf(this.options.tokens[i]);
+      if (thisTokenPos > lastTokenPos)
+        lastTokenPos = thisTokenPos;
     }
-    return last_token_pos;
+    return lastTokenPos;
   }
 }
 
 Ajax.Autocompleter = Class.create();
 Object.extend(Object.extend(Ajax.Autocompleter.prototype, Autocompleter.Base.prototype), {
   initialize: function(element, update, url, options) {
-	  this.base_initialize(element, update, options);
+	  this.baseInitialize(element, update, options);
     this.options.asynchronous  = true;
     this.options.onComplete    = this.onComplete.bind(this)
     this.options.method        = 'post';
@@ -333,7 +333,7 @@ Object.extend(Object.extend(Ajax.Autocompleter.prototype, Autocompleter.Base.pro
   
   getUpdatedChoices: function() {
     entry = encodeURIComponent(this.element.name) + '=' + 
-      encodeURIComponent(this.getEntry());
+      encodeURIComponent(this.getToken());
       
     this.options.parameters = this.options.callback ?
       this.options.callback(this.element, entry) : entry;
@@ -362,22 +362,22 @@ Object.extend(Object.extend(Ajax.Autocompleter.prototype, Autocompleter.Base.pro
 // Extra local autocompletion options:
 // - choices - How many autocompletion choices to offer
 //
-// - partial_search - If false, the autocompleter will match entered
+// - partialSearch - If false, the autocompleter will match entered
 //                    text only at the beginning of strings in the 
 //                    autocomplete array. Defaults to true, which will
 //                    match text at the beginning of any *word* in the
 //                    strings in the autocomplete array. If you want to
 //                    search anywhere in the string, additionally set
-//                    the option full_search to true (default: off).
+//                    the option fullSearch to true (default: off).
 //
-// - full_search - Search anywhere in autocomplete array strings.
+// - fullSsearch - Search anywhere in autocomplete array strings.
 //
-// - partial_chars - How many characters to enter before triggering
-//                   a partial match (unlike min_chars, which defines
+// - partialChars - How many characters to enter before triggering
+//                   a partial match (unlike minChars, which defines
 //                   how many characters are required to do any match
 //                   at all). Defaults to 2.
 //
-// - ignore_case - Whether to ignore case when autocompleting.
+// - ignoreCase - Whether to ignore case when autocompleting.
 //                 Defaults to true.
 //
 // It's possible to pass in a custom function as the 'selector' 
@@ -388,7 +388,7 @@ Object.extend(Object.extend(Ajax.Autocompleter.prototype, Autocompleter.Base.pro
 Autocompleter.Local = Class.create();
 Autocompleter.Local.prototype = Object.extend(new Autocompleter.Base(), {
   initialize: function(element, update, array, options) {
-    this.base_initialize(element, update, options);
+    this.baseInitialize(element, update, options);
     this.options.array = array;
   },
 
@@ -399,41 +399,42 @@ Autocompleter.Local.prototype = Object.extend(new Autocompleter.Base(), {
   setOptions: function(options) {
     this.options = Object.extend({
       choices: 10,
-      partial_search: true,
-      partial_chars: 2,
-      ignore_case: true,
-      full_search: false,
+      partialSearch: true,
+      partialChars: 2,
+      ignoreCase: true,
+      fullSearch: false,
       selector: function(instance) {
-        var ret       = new Array(); // Beginning matches
-        var partial   = new Array(); // Inside matches
-        var entry     = instance.getEntry();
+        var ret       = []; // Beginning matches
+        var partial   = []; // Inside matches
+        var entry     = instance.getToken();
         var count     = 0;
         
         for (var i = 0; i < instance.options.array.length &&  
-            ret.length < instance.options.choices ; i++) { 
+          ret.length < instance.options.choices ; i++) { 
+          
           var elem = instance.options.array[i];
-          var found_pos = instance.options.ignore_case ? 
+          var foundPos = instance.options.ignoreCase ? 
             elem.toLowerCase().indexOf(entry.toLowerCase()) : 
             elem.indexOf(entry);
 
-          while (found_pos != -1) {
-            if (found_pos == 0 && elem.length != entry.length) { 
+          while (foundPos != -1) {
+            if (foundPos == 0 && elem.length != entry.length) { 
               ret.push("<li><strong>" + elem.substr(0, entry.length) + "</strong>" + 
                 elem.substr(entry.length) + "</li>");
               break;
-            } else if (entry.length >= instance.options.partial_chars && 
-              instance.options.partial_search && found_pos != -1) {
-              if (instance.options.full_search || /\s/.test(elem.substr(found_pos-1,1))) {
-                partial.push("<li>" + elem.substr(0, found_pos) + "<strong>" +
-                  elem.substr(found_pos, entry.length) + "</strong>" + elem.substr(
-                  found_pos + entry.length) + "</li>");
+            } else if (entry.length >= instance.options.partialChars && 
+              instance.options.partialSearch && foundPos != -1) {
+              if (instance.options.fullSearch || /\s/.test(elem.substr(foundPos-1,1))) {
+                partial.push("<li>" + elem.substr(0, foundPos) + "<strong>" +
+                  elem.substr(foundPos, entry.length) + "</strong>" + elem.substr(
+                  foundPos + entry.length) + "</li>");
                 break;
               }
             }
 
-            found_pos = instance.options.ignore_case ? 
-              elem.toLowerCase().indexOf(entry.toLowerCase(), found_pos + 1) : 
-              elem.indexOf(entry, found_pos + 1);
+            foundPos = instance.options.ignoreCase ? 
+              elem.toLowerCase().indexOf(entry.toLowerCase(), foundPos + 1) : 
+              elem.indexOf(entry, foundPos + 1);
 
           }
         }
