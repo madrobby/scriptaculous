452a7d2fc69d26655c92037f11896d6287b730fd
Thomas Fuchs <thomas@fesch.at>
2005-09-03 18:40:00 +0000

    script.aculo.us: V1.5_pre4
    git-svn-id: http://svn.rubyonrails.org/rails/spinoffs/scriptaculous@2124 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- src/scriptaculous.js
+++ src/scriptaculous.js
@@ -1,5 +1,5 @@
 var Scriptaculous = {
-  Version: '1.5_pre4_trunk',
+  Version: '1.5_pre4',
   require: function(libraryName) {
     // inserting via DOM fails in Safari 2.0, so brute force approach
     document.write('<script type="text/javascript" src="'+libraryName+'"></script>');
