3ff9b97c169a9f5340d12f7a5848e5af5de3dcdb
Thomas Fuchs <thomas@fesch.at>
2006-08-29 19:57:14 +0000

    script.aculo.us: Fix selection of correct option in SELECT element generated by InPlaceCollectionEditor for indexed option arrays, fixes #4789 [thx steve]
    git-svn-id: http://svn.rubyonrails.org/rails/spinoffs/scriptaculous@4852 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- src/controls.js
+++ src/controls.js
@@ -783,6 +783,8 @@ Object.extend(Ajax.InPlaceCollectionEditor.prototype, {
       collection.each(function(e,i) {
         optionTag = document.createElement("option");
         optionTag.value = (e instanceof Array) ? e[0] : e;
+        if((typeof this.options.value == 'undefined') && 
+          ((e instanceof Array) ? this.element.innerHTML == e[1] : e == optionTag.value)) optionTag.selected = true;
         if(this.options.value==optionTag.value) optionTag.selected = true;
         optionTag.appendChild(document.createTextNode((e instanceof Array) ? e[1] : e));
         selectTag.appendChild(optionTag);
