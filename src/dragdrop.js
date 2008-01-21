47fa18a622fabc87248b193fff833393d7c2982e
Thomas Fuchs <thomas@fesch.at>
2008-01-21 13:36:00 +0000

    Fixes an issue with IE ghosting on non-absolute elements. Closes #10423.
    git-svn-id: http://svn.rubyonrails.org/rails/spinoffs/scriptaculous@8680 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- src/dragdrop.js
+++ src/dragdrop.js
@@ -331,8 +331,8 @@ var Draggable = Class.create({
     
     if(this.options.ghosting) {
       this._clone = this.element.cloneNode(true);
-      this.element._originallyAbsolute = (this.element.getStyle('position') == 'absolute');
-      if (!this.element._originallyAbsolute)
+      this._originallyAbsolute = (this.element.getStyle('position') == 'absolute');
+      if (!this._originallyAbsolute)
         Position.absolutize(this.element);
       this.element.parentNode.insertBefore(this._clone, this.element);
     }
@@ -403,9 +403,9 @@ var Draggable = Class.create({
     }
 
     if(this.options.ghosting) {
-      if (!this.element._originallyAbsolute)
+      if (!this._originallyAbsolute)
         Position.relativize(this.element);
-      delete this.element._originallyAbsolute;
+      delete this._originallyAbsolute;
       Element.remove(this._clone);
       this._clone = null;
     }
