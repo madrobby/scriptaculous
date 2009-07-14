d901c090d960eaea7d9bd7f2222064638a7edaac
Thomas Fuchs <thomas@fesch.at>
2009-07-14 10:24:44 +0200

    Update copyright msgs

--- src/dragdrop.js
+++ src/dragdrop.js
@@ -1,5 +1,4 @@
-// Copyright (c) 2005-2008 Thomas Fuchs (http://script.aculo.us, http://mir.aculo.us)
-//           (c) 2005-2008 Sammi Williams (http://www.oriontransfer.co.nz, sammi@oriontransfer.co.nz)
+// Copyright (c) 2005-2009 Thomas Fuchs (http://script.aculo.us, http://mir.aculo.us)
 //
 // script.aculo.us is freely distributable under the terms of an MIT-style license.
 // For details, see the script.aculo.us web site: http://script.aculo.us/
